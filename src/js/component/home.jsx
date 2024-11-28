da7a21bff1840d9e5830ea57efc88753ce0da76e
jorgelozano25 <jorge_9dealmeria@hotmail.com>
2024-11-28 19:34:35 +0000

    terminado

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  const [selectColor, setSelectColor] = useState("red");
+  const [lights, setLights] = useState(["red", "yellow", "green"]);
+
+  // Function para cambiar de color
+  const cycleColors = () => {
+    const currentIndex = lights.indexOf(selectColor);
+    const nextIndex = (currentIndex + 1) % lights.length;
+    setSelectColor(lights[nextIndex]);
+  };
+
+  // Function añadir morado
+  const togglePurpleLight = () => {
+    if (lights.includes("purple")) {
+      setLights(lights.filter((color) => color !== "purple"));
+    } else {
+		setLights([...lights, "purple"])
+	}
+  };
+
+  return (
+    <div className="d-flex justify-content-center">
+      <div className="traffic-light">
+        <div
+          onClick={() => setSelectColor("red")}
+          className={"light red " + (selectColor === "red" ? " glow" : "")}
+        ></div>
+        <div
+          onClick={() => setSelectColor("yellow")}
+          className={"light yellow " + (selectColor === "yellow" ? " glow" : "")}
+        ></div>
+        <div
+          onClick={() => setSelectColor("green")}
+          className={"light green " + (selectColor === "green" ? " glow" : "")}
+        ></div>
+        {lights.includes("purple") && (
+          <div
+            onClick={() => setSelectColor("purple")}
+            className={"light purple " + (selectColor === "purple" ? " glow" : "")}
+          ></div>
+        )}
+      </div>
+      <div className="mt-3">
+        <button onClick={cycleColors} className="btn btn-primary mx-2">
+          Cambiar de color
+        </button>
+        <button onClick={togglePurpleLight} className={"btn mx-2 " +(lights.includes("purple") ? "btn-danger" : "btn-success")}>
+          {lights.includes("purple") ? "Quitar Morado" : "Añadir Morado"}
+        </button> 
+      </div>
+    </div>
+  );
 };
 
 export default Home;
